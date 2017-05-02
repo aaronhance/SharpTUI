f71a064357b02d7c5aa76000e47d3899ab55d626
aaronhance <1man2guns3kills@gmail.com>
2017-05-02 18:36:09 +0100

    Update AssemblyInfo.cs

--- STUI/Properties/AssemblyInfo.cs
+++ STUI/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SharpTUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SharpTUI")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © Aaron Hance 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
